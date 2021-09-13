6c82fab5ce49edbe0b850876b7786d53cee70169
seedsat <e.satoshi.bi@gmail.com>
2021-09-13 20:56:39 +0900

    課題提出テスト
    フォームに入力した内容を取得する

---
+++ 2-11/result.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST['my_name'];
+$email = $_POST['email'];
+$pwd = $_POST['password'];
+
+echo "私の名前は、{$name}<br>";
+echo "メールアドレスは、{$email}<br>";
+echo "私のパスワードは、{$pwd}";
+
+?>
