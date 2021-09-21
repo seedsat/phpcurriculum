58ac495808bf3809cb557b75ee3b9018679849f4
seedsat <e.satoshi.bi@gmail.com>
2021-09-22 00:36:01 +0900

    課題提出テスト
    第2章チェックテスト課題修正

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -8,24 +8,8 @@ $a_port     = $_POST['a-port'];
 $a_language = $_POST['a-language'];
 $a_command  = $_POST['a-command'];
 
-function port($port, $a_port) {
-  if($port == $a_port) {
-    echo '正解！';
-  } else {
-    echo '残念・・・';
-  }
-}
-
-function language($language, $a_language) {
-  if($language == $a_language) {
-    echo '正解！';
-  } else {
-    echo '残念・・・';
-  }
-}
-
-function command($command, $a_command) {
-  if($command == $a_command) {
+function checkAnswer($select, $answer) {
+  if($select == $answer) {
     echo '正解！';
   } else {
     echo '残念・・・';
@@ -40,17 +24,17 @@ function command($command, $a_command) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>Document</title>
+  <title>答え合わせ</title>
 </head>
 <body>
   <div class="answer">
     <p><?php echo $name; ?>さんの結果は・・・？</p>
     <p>①の答え</p>
-    <?php port($port, $a_port); ?>
+    <?php checkAnswer($port, $a_port); ?>
     <p>②の答え</p>
-    <?php language($language, $a_language); ?>
+    <?php checkAnswer($language, $a_language); ?>
     <p>③の答え</p>
-    <?php command($command, $a_command); ?>
+    <?php checkAnswer($command, $a_command); ?>
   </div>
 </body>
 </html>
--- 2-18/question.php
+++ 2-18/question.php
@@ -1,13 +1,13 @@
 <?php
-  $name = $_POST['name'];
+  $name = $_POST['name'] ? $_POST['name'] : 'ゲスト';
 
-  $port = [1 =>'80', 2 => '22', 3 => '20', 4 =>'21'];
-  $language = [1 => 'PHP', 2 => 'Python', 3 => 'JAVA', 4 => 'HTML'];
-  $command = [1 => 'join', 2 => 'select', 3 => 'insert', 4 => 'update'];
+  $port = ['80', '22', '20','21'];
+  $language = [ 'PHP', 'Python', 'JAVA', 'HTML'];
+  $command = [ 'join', 'select', 'insert', 'update'];
 
-  $a_port = 1;
-  $a_language = 1;
-  $a_command = 2;
+  $a_port = 0;
+  $a_language = 3;
+  $a_command = 1;
 
 ?>
 <!DOCTYPE html>
@@ -17,7 +17,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>Document</title>
+  <title>問題</title>
 </head>
 <body>
   <div class="question">
